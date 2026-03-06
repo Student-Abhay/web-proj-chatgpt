3eb03a8c78002f387d46d390d528206e2149eb8e
Student-Abhay <gadgeabhay098@gmail.com>
2026-03-06 18:09:02 +0530

    fix api baseURL

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://web-proj-chatgpt-production.up.railway.app/test-db",
+  baseURL: "https://web-proj-chatgpt-production.up.railway.app/api",
 });
 
 // Attach JWT token to every request
